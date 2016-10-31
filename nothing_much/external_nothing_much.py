c9298a34af6026bdddea3a097de7d24ba3dd5f9b
ianneilmacleod <ian.macleod@geosoft.com>
2016-10-31 17:24:30 -0400

    Nothing much

--- nothing_much/external_nothing_much.py
+++ nothing_much/external_nothing_much.py
@@ -1,10 +1,10 @@
-import pydevd
+#import pydevd
 import os
 import geosoft.gxpy.utility as gxu
 
 def rungx():
 
-    pydevd.settrace('localhost', port=34765, stdoutToServer=True, stderrToServer=True)
+    #pydevd.settrace('localhost', port=34765, stdoutToServer=True, stderrToServer=True)
 
     script = os.path.join(os.path.split(__file__)[0], 'nothing_much.py')
     gxu.run_external_python(script)
